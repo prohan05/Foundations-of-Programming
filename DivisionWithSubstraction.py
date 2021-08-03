506ed22ba1f50ec7043e4c40e3be6ffbb104ba0f
Rohan Pathak <84720598+prohan05@users.noreply.github.com>
2021-08-03 14:28:52 +0530

    Update DivisionWithSubstraction.py

--- DivisionWithSubstraction.py
+++ DivisionWithSubstraction.py
@@ -1,13 +1,19 @@
-# This is written in Python3 language
+# This is written in Python3
 def new_division(dividend, divisor):
     quotient = 0
     # Repetitive substraction as follows
-    while dividend >= divisor:
-        dividend = dividend - divisor
-        quotient += 1
-    remainder = dividend
-    print('Quotient: ', quotient)
-    print('remainder: ', remainder)
+    if divisor != 0:
+        while dividend >= divisor:
+            if divisor != 0:
+                dividend = dividend - divisor
+                quotient += 1
+        remainder = dividend
+        print('Quotient: ', quotient)
+        print('remainder: ', remainder)
+    else:
+        print('Division is not possible')
 
 
+new_division(7, 3)
 new_division(14, 3)
+new_division(15, 0)
